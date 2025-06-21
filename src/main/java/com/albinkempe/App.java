2dcdca1e9d9ce67ad116bab5e770d5e852c0501a
Albin Kempe <albin.kempe@outlook.com>
2025-06-21 12:04:15 +0200

    Added add function and test

--- src/main/java/com/albinkempe/App.java
+++ src/main/java/com/albinkempe/App.java
@@ -6,5 +6,10 @@ package com.albinkempe;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Sum: " + add(2, 3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/com/albinkempe/AppTest.java
+++ src/test/java/com/albinkempe/AppTest.java
@@ -1,5 +1,6 @@
 package com.albinkempe;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,13 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+        assertEquals(1, App.add(1, 0));
+        assertEquals(0, App.add(0, 0));
+        assertEquals(-1, App.add(-2, 1));
     }
 }
